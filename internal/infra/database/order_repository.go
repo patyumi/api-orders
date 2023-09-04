c47646551259dc0706845f4e1803602f0420c774
Patricia Yumi <pati.yumi@yahoo.com.br>
2023-09-04 17:36:56 -0300

    Create table orders

--- internal/infra/database/order_repository.go
+++ internal/infra/database/order_repository.go
@@ -12,7 +12,17 @@ type OrderRepository struct {
 }
 
 func NewOrderRepository(db *sql.DB) *OrderRepository {
+	if db != nil {
+		createOrderTable(db)
+	}
 	return &OrderRepository{Db: db}
+}
+
+func createOrderTable(db *sql.DB) {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func (r *OrderRepository) Save(order *entity.Order) error {
